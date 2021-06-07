b5de58d21ba4095ba33f7c8373c510a3b97fa581
alllef <alekz29@gmail.com>
2021-06-07 15:55:16 +0300

    Initial commit

---
+++ src/test/java/com/github/alllef/AddToCartTest.java
@@ -0,0 +1,13 @@
+package com.github.alllef;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class AddToCartTest {
+
+}
