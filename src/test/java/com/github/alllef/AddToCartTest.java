6f42557c67fa6f801468940a42120fdc13116a69
alllef <alekz29@gmail.com>
2021-06-07 16:02:06 +0300

    added classes

--- src/test/java/com/github/alllef/AddToCartTest.java
+++ src/test/java/com/github/alllef/AddToCartTest.java
@@ -9,5 +9,35 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.util.concurrent.TimeUnit;
 
 public class AddToCartTest {
+    public static MainPage mainPage;
+    public static WebDriver driver;
 
+    @BeforeClass
+    public static void setup() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        driver = new ChromeDriver();
+        mainPage = new MainPage(driver);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://dominos.ua/en/kyiv/");
+    }
+
+    public void mainPageCartTest(int addNumber) {
+        mainPage.addToCart(addNumber);
+
+        String expectedQuantityResult = "0" + addNumber;
+        if (addNumber >= 10) expectedQuantityResult = String.valueOf(addNumber);
+
+        Assert.assertEquals("Quantity of cart is not the same as expected quantity", expectedQuantityResult, mainPage.getCartQuantity());
+        Assert.assertEquals("Price in cart is not the same as expected price", mainPage.getPrice(addNumber), mainPage.getCartPrice(), 0.1);
+    }
+
+
+    @Test
+    public void testMainPageCart() {
+        mainPageCartTest(5);
+        mainPageCartTest(3);
+        mainPageCartTest(0);
+        mainPageCartTest(11);
+    }
 }
--- src/test/java/com/github/alllef/MainPage.java
+++ src/test/java/com/github/alllef/MainPage.java
@@ -6,5 +6,70 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class MainPage {
-    
+    @FindBy(css = "#root > div.content > div.dp-product-list__container > div:nth-child(2) > div.dp-product-list > div:nth-child(1) > div > div.dp-product-block__description > div.dp-product-block__price-row > div.dp-product-block__btn-wrap > button")
+    private WebElement chickenKebabToCartBtn;
+
+    @FindBy(css = "#root > div.content > div.dp-product-list__container > div:nth-child(2) > div.dp-product-list > div:nth-child(1) > div > div.dp-product-block__description > div.dp-product-block__price-row > div.dp-product-block__btn-wrap > div > button:nth-child(3)")
+    private WebElement chickenKebabAddMoreToCartBtn;
+
+    @FindBy(css = "#root > div.content > div.dp-product-list__container > div:nth-child(2) > div.dp-product-list > div:nth-child(1) > div > div.dp-product-block__description > div.dp-product-block__price-row > div.dp-product-block__btn-wrap > div > button:nth-child(1)")
+    private WebElement chickenKebabRemoveFromCartBtn;
+
+    @FindBy(css = "#root > div.content > div.dp-product-list__container > div:nth-child(2) > div.dp-product-list > div:nth-child(1) > div > div.dp-product-block__description > div.dp-product-block__price-row > div.dp-product-block__price-block > div > span.dp-product-block__price")
+    private WebElement chickenKebabPrice;
+
+    @FindBy(css = "#root > div.dp-header-desktop > div.main-header.main-header_sticky > div > div.main-header__cart > div > div > div > div > div")
+    private WebElement cartQuantity;
+
+    @FindBy(css = "#root > div.dp-header-desktop > div.main-header.main-header_sticky > div > div.main-header__cart > div > div > div > div.button-cart__info-price > p")
+    private WebElement cartPrice;
+
+    private int cartNumber = 0;
+
+    public WebDriver driver;
+
+    public MainPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+        this.driver = driver;
+    }
+
+    public void addToCart(int number) {
+        if (cartNumber > number) {
+            while (cartNumber != number) {
+                chickenKebabRemoveFromCartBtn.click();
+                cartNumber--;
+            }
+        } else {
+
+            if (cartNumber == 0 && number != 0) {
+                chickenKebabToCartBtn.click();
+                cartNumber++;
+            }
+
+            while (cartNumber != number) {
+                chickenKebabAddMoreToCartBtn.click();
+                cartNumber++;
+            }
+        }
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public double getPrice(int number) {
+        return Double.parseDouble(chickenKebabPrice.getText()) * number;
+    }
+
+    public double getCartPrice() {
+        if (cartNumber == 0) return 0;
+        return Double.parseDouble(cartPrice.getText().split(" ")[0]);
+    }
+
+    String getCartQuantity() {
+        return cartQuantity.getText();
+    }
 }
